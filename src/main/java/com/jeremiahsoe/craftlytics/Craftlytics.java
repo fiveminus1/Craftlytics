0b97cbf22a1ccde049003289a7c6acc259e51d2d
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2024-12-27 02:35:12 -0800

    biomeexploration event logged. doesn't work because need to log new players

--- src/main/java/com/jeremiahsoe/craftlytics/Craftlytics.java
+++ src/main/java/com/jeremiahsoe/craftlytics/Craftlytics.java
@@ -1,17 +1,40 @@
 package com.jeremiahsoe.craftlytics;
 
+import com.jeremiahsoe.craftlytics.database.DatabaseManager;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Craftlytics extends JavaPlugin {
+
+public final class Craftlytics extends JavaPlugin implements Listener {
+
+    private DatabaseManager databaseManager;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        databaseManager = new DatabaseManager();
+        databaseManager.init();
 
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent event){
+        System.out.println("logging player move event");
+        String playerUuid = event.getPlayer().getUniqueId().toString();
+        String biomeName = event.getTo().getBlock().getBiome().toString();
+        String location = event.getTo().getBlockX() + "," +  event.getTo().getBlockY() + "," + event.getTo().getBlockZ();
+
+        databaseManager.logBiomeExploration(playerUuid, biomeName, location);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        if (databaseManager != null){
+            databaseManager.close();
+        }
     }
 }
--- src/main/java/com/jeremiahsoe/craftlytics/database/DatabaseManager.java
+++ src/main/java/com/jeremiahsoe/craftlytics/database/DatabaseManager.java
@@ -4,24 +4,42 @@ import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseManager {
     private static HikariDataSource dataSource;
 
-    public static void init(){
+    public void init(){
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl("jdbc:postgresql://localhost:5432/craftlytics");
         config.setUsername("craftlytics_user");
         config.setPassword("6346");
+        config.setDriverClassName(org.postgresql.Driver.class.getName());
         dataSource = new HikariDataSource(config);
     }
 
-    public static Connection getConnection() throws SQLException{
+    public Connection getConnection() throws SQLException{
         return dataSource.getConnection();
     }
 
-    public static void close(){
+    public void logBiomeExploration(String playerUuid, String biomeName, String location){
+        String sql = "INSERT INTO Biomes_Explored (player_uuid, biome_name, location) VALUES (?,?,?)";
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement statement = conn.prepareStatement(sql)) {
+
+            statement.setString(1, playerUuid);
+            statement.setString(2, biomeName);
+            statement.setString(3, location);
+            statement.executeUpdate();
+        }
+        catch (SQLException e){
+            e.printStackTrace(); //edit later
+        }
+
+    }
+
+    public void close(){
         if(dataSource != null){
             dataSource.close();
         }
