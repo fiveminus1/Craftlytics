68133b5a9b589e94b13c4dbb9849d5339c20bffc
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2024-12-27 19:29:39 -0800

    databasemanager updated to use singleton pattern. additionally, removed biome service from databasemanager to separate into separate file later

--- src/main/java/com/jeremiahsoe/craftlytics/database/DatabaseManager.java
+++ src/main/java/com/jeremiahsoe/craftlytics/database/DatabaseManager.java
@@ -8,7 +8,17 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static HikariDataSource dataSource;
+    private static DatabaseManager instance;
+    private HikariDataSource dataSource;
+
+    private DatabaseManager(){}
+
+    public static synchronized DatabaseManager getInstance(){
+        if (instance == null){
+            instance = new DatabaseManager();
+        }
+        return instance;
+    }
 
     public void init(){
         HikariConfig config = new HikariConfig();
@@ -23,21 +33,21 @@ public class DatabaseManager {
         return dataSource.getConnection();
     }
 
-    public void logBiomeExploration(String playerUuid, String biomeName, String location){
-        String sql = "INSERT INTO Biomes_Explored (player_uuid, biome_name, location) VALUES (?,?,?)";
-        try (Connection conn = dataSource.getConnection();
-             PreparedStatement statement = conn.prepareStatement(sql)) {
-
-            statement.setString(1, playerUuid);
-            statement.setString(2, biomeName);
-            statement.setString(3, location);
-            statement.executeUpdate();
-        }
-        catch (SQLException e){
-            e.printStackTrace(); //edit later
-        }
-
-    }
+//    public void logBiomeExploration(String playerUuid, String biomeName, String location){
+//        String sql = "INSERT INTO Biomes_Explored (player_uuid, biome_name, location) VALUES (?,?,?)";
+//        try (Connection conn = dataSource.getConnection();
+//             PreparedStatement statement = conn.prepareStatement(sql)) {
+//
+//            statement.setString(1, playerUuid);
+//            statement.setString(2, biomeName);
+//            statement.setString(3, location);
+//            statement.executeUpdate();
+//        }
+//        catch (SQLException e){
+//            e.printStackTrace(); //edit later
+//        }
+//
+//    }
 
     public void close(){
         if(dataSource != null){
